892c1b9782d415fd2929b99ed75fc2015e55cbc0
kscub@Satya17 <kscuber4646@gmail.com>
2023-07-22 01:03:17 +0530

    Classes learnt

--- classes.js
+++ classes.js
@@ -1,1 +1,32 @@
+/*Factory function
+function pizzaFactory(pizzaSize){
+    const crust ="original";
+    const size = pizzaSize;
+    return {
+        bake: ()=>console.log(`baking a ${size} ${crust} crust pizza.`)
+    };
+}
+const myPizza = pizzaFactory("small");
+myPizza.bake();
+*/
+
+
+
 //classes
+class Pizza{
+    crust = "original" //public field
+    #sauce = "traditonal" // private field
+    constructor(pizzaSize){
+    this.size=pizzaSize;
+    //this.crust="original";
+    }
+    getCrust(){
+        return this.crust;
+    }
+    setCrust(pizzaCrust){
+        this.crust=pizzaCrust;
+    }
+   
+}
+
+
